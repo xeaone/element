9bb0adb85053d61ceb96a6f9635da40f4df93157
Alexander Elias <alex.steven.elias@gmail.com>
2017-11-14 14:53:20 -0700

    fixed wrong location.origin

--- src/router.js
+++ src/router.js
@@ -172,7 +172,7 @@ Router.toLocation = function (path) {
 	var location = {};
 
 	location.pathname = decodeURI(path);
-	location.origin = Global.location.origin;
+	location.origin = window.location.origin;
 	location.base = this.base ? this.base : location.origin;
 
 	location.port = window.location.port;
