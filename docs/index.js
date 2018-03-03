b793edd8b44b65075ff4f28cb3b15d29b838ece8
Alexander Elias <alex.steven.elias@gmail.com>
2018-03-02 18:21:50 -0700

    3.4.5

--- docs/index.js
+++ docs/index.js
@@ -2,7 +2,6 @@ import './assets/prism.js';
 
 import cMenu from './components/c-menu.js';
 import cFoo from './components/c-foo.js';
-
 import rIndex from './routes/r-index.js';
 import rTest from './routes/r-test.js';
 import r404 from './routes/r-404.js';
--- docs/routes/r-test.js
+++ docs/routes/r-test.js
@@ -69,17 +69,6 @@ export default {
 						}
 					}
 				}
-			},
-			s1: function () {
-				console.log(arguments);
-				return {
-					method: 'get',
-					action: '/data.json',
-					handler: function () {
-						console.log(arguments);
-						// e.target.reset();
-					}
-				}
 			}
 		},
 		properties: {
@@ -101,25 +90,18 @@ export default {
 
 			var total = self.add(1, 2, 3);
 
-			// setTimeout(function () {
-			// 	console.log(self.model.items.shift());
-			// }, 1000);
-
 			setTimeout(function () {
-
 				var increaseInterval = setInterval(function () {
 
 					if (self.model.items.length === 10) {
 						clearInterval(increaseInterval);
 
 						var decreaseInterval = setInterval(function () {
-
 							if (self.model.items.length === 5) {
 								clearInterval(decreaseInterval);
 							} else {
 								self.model.items.pop();
 							}
-
 						}, 6);
 
 					} else {
@@ -127,140 +109,127 @@ export default {
 					}
 
 				}, 6);
-
 			}, 3000);
 
 			Say('r-test created');
 		},
-		// template: function (render) {
-		// 	setTimeout(function () {
-		// 		render(template)
-		// 	}, 0);
-		// }
 		template: `
-			<style>
-				[o-each-item] {
-					min-height: 150px;
-				}
-			</style>
-			<br>
-			<br>
+		<style>
+			[o-each-item] {
+				min-height: 150px;
+			}
+		</style>
+		<br>
+		<br>
 
-			<strong o-text="nah">nah</strong>
-			<strong o-show="isshow">isshow</strong>
-			<strong o-hide="ishide">ishide</strong>
+		<strong o-text="nah">nah</strong>
+		<strong o-show="isshow">isshow</strong>
+		<strong o-hide="ishide">ishide</strong>
 
-			<form o-submit="s0" o-reset>
-				<div o-text="loopy.doopy"></div>
-				<input type="text" o-value="loopy.doopy" placeholder="text" required><br>
-				<input type="text" o-value="blank" placeholder="text" required><br>
-				<input type="submit" name="Submit">
-			</form>
-			<br>
-			<br>
+		<form o-submit="s0" o-reset>
+			<div o-text="loopy.doopy"></div>
+			<input type="text" o-value="loopy.doopy" placeholder="text" required><br>
+			<input type="text" o-value="blank" placeholder="text" required><br>
+			<input type="submit" name="Submit">
+		</form>
+		<br>
+		<br>
 
-			<form o-submit="s1">
-				<input type="submit" name="Submit">
-			</form>
-			<br>
-			<br>
+		<div o-show="show_hide">Now you see me!</div>
+		<button o-on-click="toggle_show_hide">Show/Hide</button>
+		<br>
+		<br>
 
-			<div o-show="show_hide">Now you see me!</div>
-			<button o-on-click="toggle_show_hide">Show/Hide</button>
-			<br>
-			<br>
+		<c-menu>
+			<li slot="one">Item One</li>
+			<li slot="two">Item Two</li>
+		</c-menu>
+		<br>
+		<br>
 
-			<c-menu>
-				<li slot="one">Item One</li>
-				<li slot="two">Item Two</li>
-			</c-menu>
-			<br>
-			<br>
+		<c-menu>
+			<li>Item Three</li>
+			<li>Item Four</li>
+		</c-menu>
+		<br>
+		<br>
 
-			<c-menu>
-				<li>Item Three</li>
-				<li>Item Four</li>
-			</c-menu>
-			<br>
-			<br>
+		<p o-text="text | upper"></p>
+		<p o-text="text | lower"></p>
+		<input type="text" o-value="text | lower" placeholder="text">
+		<input type="text" o-value="text | upper" placeholder="text">
+		<br>
+		<br>
 
-			<p o-text="text | upper"></p>
-			<p o-text="text | lower"></p>
-			<input type="text" o-value="text | lower" placeholder="text">
-			<input type="text" o-value="text | upper" placeholder="text">
-			<br>
-			<br>
+		<div o-text="isChecked"></div>
+		<input type="checkbox" o-value="isChecked">
+		<br>
+		<br>
 
-			<div o-text="isChecked"></div>
-			<input type="checkbox" o-value="isChecked">
-			<br>
-			<br>
+		<div o-text="initiallyNotOnModel">initiallyNotOnModel</div>
+		<input type="checkbox" o-value="initiallyNotOnModel">
+		<br>
+		<br>
 
-			<div o-text="initiallyNotOnModel">initiallyNotOnModel</div>
-			<input type="checkbox" o-value="initiallyNotOnModel">
-			<br>
-			<br>
+		<div o-text="numRadio"></div>
+		<input type="radio" o-value="numRadio">
+		<input type="radio" o-value="numRadio">
+		<br>
+		<br>
 
-			<div o-text="numRadio"></div>
-			<input type="radio" o-value="numRadio">
-			<input type="radio" o-value="numRadio">
-			<br>
-			<br>
+		<div o-text="car"></div>
+		<select o-value="car">
+			<option value="audi">audi</option>
+			<option value="saab" selected>saab</option>
+			<option value="volvo">volvo</option>
+			<option value="mercedes">mercedes</option>
+		</select>
+		<br>
+		<br>
 
-			<div o-text="car"></div>
-			<select o-value="car">
-				<option value="audi">audi</option>
-				<option value="saab" selected>saab</option>
-				<option value="volvo">volvo</option>
-				<option value="mercedes">mercedes</option>
-			</select>
-			<br>
-			<br>
+		<div o-text="cars"></div>
+		<select o-value="cars" o-value="cars" o-each-onecar="allcars" multiple>
+			<option o-value="onecar" o-text="onecar"></option>
+		</select>
+		<br>
+		<br>
 
-			<div o-text="cars"></div>
-			<select o-value="cars" o-value="cars" o-each-onecar="allcars" multiple>
-				<option o-value="onecar" o-text="onecar"></option>
-			</select>
-			<br>
-			<br>
+		<div o-text="items.0.it.val"></div>
+		<input type="text" o-value="items.0.it.val">
+		<div o-each-item="items">
+			<span>
+				<span>$item</span>
+				<span o-on-click="foo" o-text="item.it.val"></span>
+				<span>,</span>
+			</span>
+		</div>
 
-			<div o-text="items.0.it.val"></div>
-			<input type="text" o-value="items.0.it.val">
-			<div o-each-item="items">
-				<span>
-					<span>$item</span>
-					<span o-on-click="foo" o-text="item.it.val"></span>
-					<span>,</span>
-				</span>
-			</div>
+		<button o-on-click="say">Console Log</button>
+		<br>
+		<br>
 
-			<button o-on-click="say">Console Log</button>
-			<br>
-			<br>
+		<ul>
+			<li>
+				<a href="test/">test</a>
+			</li>
+			<li>
+				<a href="js">js</a>
+			</li>
+			<li>
+				<a href="js/?name=ferret&color=purple#hash">js/?name=ferret&amp;color=purple#hash</a>
+			</li>
+			<li>
+				<a href="https://google.com/">google</a>
+			</li>
+			<li>
+				<a href="https://google.com/" external>google external</a>
+			</li>
+			<li>
+				<a href="https://google.com/" target="_blank">google target_blank</a>
+			</li>
+		</ul>
 
-			<ul>
-				<li>
-					<a href="test/">test</a>
-				</li>
-				<li>
-					<a href="js">js</a>
-				</li>
-				<li>
-					<a href="js/?name=ferret&color=purple#hash">js/?name=ferret&amp;color=purple#hash</a>
-				</li>
-				<li>
-					<a href="https://google.com/">google</a>
-				</li>
-				<li>
-					<a href="https://google.com/" external>google external</a>
-				</li>
-				<li>
-					<a href="https://google.com/" target="_blank">google target_blank</a>
-				</li>
-			</ul>
-
-			<div o-html="html"></div>
-
+		<div o-html="html"></div>
 		`
 	}
 }
