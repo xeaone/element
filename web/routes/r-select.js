c56d546a58068b8ec262ee61b7742e072c563317
Alexander Elias <alex.steven.elias@gmail.com>
2018-10-03 16:51:05 -0700

    added another select

--- web/routes/r-select.js
+++ web/routes/r-select.js
@@ -15,22 +15,40 @@ export default {
 				'Volvo',
 				'Mercedes',
 			],
+			names: [
+				'jon',
+				'alex',
+				'bill',
+				'dave'
+			],
+			result: {
+				name: ''
+			}
 		},
 		template: `
 
 		<div o-text="car"></div>
 		<select o-value="car">
 			<option value="audi">audi</option>
-			<option value="saab" selected>saab</option>
 			<option value="volvo">volvo</option>
 			<option value="mercedes">mercedes</option>
+			<option value="saab" selected>saab</option>
 		</select>
 
 		<hr>
 
 		<div o-text="cars"></div>
-		<select o-value="cars" o-value="cars" o-each-onecar="allcars" multiple>
+		<select o-value="cars" o-each-onecar="allcars" multiple>
 			<option o-value="onecar" o-text="onecar"></option>
+		</select>
+
+		<hr>
+
+		<div o-text="result.name"></div>
+		// <label>Select Name</label>
+		// o-label="Select Name" 
+		<select o-value="result.name" o-each-name="names">
+			<option o-value="name" o-text="name"></option>
 		</select>
 
 		`
