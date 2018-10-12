c6d7974abd07911b8917aed55016eb542ff3b52d
Alexander Elias <alex.steven.elias@gmail.com>
2018-10-12 15:00:44 -0700

    fixed polly fill not loading correctly

--- src/index.js
+++ src/index.js
@@ -22,8 +22,6 @@ const eStyle = document.createElement('style');
 const tStyle = document.createTextNode(' \
 	o-router, o-router > :first-child { \
 		display: block; \
-	} \
-	o-router, [o-scope] { \
 		animation: o-transition 150ms ease-in-out; \
 	} \
 	@keyframes o-transition { \
@@ -36,29 +34,25 @@ eStyle.setAttribute('type', 'text/css');
 eStyle.appendChild(tStyle);
 document.head.appendChild(eStyle);
 
-let currentCount = 0;
-let requiredCount = 0;
-let loadedCalled = false;
+const oSetup = document.querySelector('script[o-setup]');
 
-const loaded = function () {
-	if (loadedCalled) return;
-	if (currentCount !== requiredCount) return;
+if (oSetup) {
 
-	loadedCalled = true;
+	let currentCount = 0;
+	let requiredCount = 0;
 
-	document.addEventListener('load', Load, true);
-	document.addEventListener('input', Input, true);
-	document.addEventListener('reset', Reset, true);
-	document.addEventListener('click', Click, true);
-	document.addEventListener('submit', Submit, true);
-	document.addEventListener('change', Change, true);
-	window.addEventListener('popstate', State, true);
+	const loaded = function () {
+		if (currentCount !== requiredCount) return;
 
-	const element = document.querySelector('script[o-setup]');
+		document.addEventListener('load', Load, true);
+		document.addEventListener('input', Input, true);
+		document.addEventListener('reset', Reset, true);
+		document.addEventListener('click', Click, true);
+		document.addEventListener('submit', Submit, true);
+		document.addEventListener('change', Change, true);
+		window.addEventListener('popstate', State, true);
 
-	if (element) {
-
-		const args = element.getAttribute('o-setup').split(/\s*,\s*/);
+		const args = oSetup.getAttribute('o-setup').split(/\s*,\s*/);
 		const meta = document.querySelector('meta[name="oxe"]');
 
 		if (meta && meta.hasAttribute('compiled')) {
@@ -73,7 +67,6 @@ const loaded = function () {
 			throw new Error('Oxe - o-setup attribute requires a url');
 		}
 
-
 		if (args.length > 1) {
 			Loader.load({
 				url: args[0],
@@ -85,56 +78,55 @@ const loaded = function () {
 			index.setAttribute('src', args[0]);
 			index.setAttribute('async', 'true');
 			index.setAttribute('type', 'module');
-			element.insertAdjacentElement('afterend', index);
+			document.head.appendChild(index);
 		}
 
-	}
+		document.registerElement('o-router', {
+			prototype: Object.create(HTMLElement.prototype)
+		});
 
-	document.registerElement('o-router', {
-		prototype: Object.create(HTMLElement.prototype)
-	});
+	};
 
-};
+	const loader = function (url, callback) {
+		const polly = document.createElement('script');
 
-const loader = function (condition, url) {
-	if (condition) {
-		requiredCount++;
-		var polly = document.createElement('script');
 		polly.setAttribute('async', 'true');
 		polly.setAttribute('src', url);
 		polly.addEventListener('load', function () {
 			currentCount++;
-			loaded();
+			callback();
 		}, true);
+
 		document.head.appendChild(polly);
-	} else {
-		loaded();
+	};
+
+	const features = [];
+	const isNotFetch = !('fetch' in window);
+	const isNotAssign = !('assign' in Object);
+	const isNotPromise = !('Promise' in window);
+	const isNotCustomElement = !('registerElement' in document) || !('content' in document.createElement('template'));
+
+	if (isNotFetch) features.push('fetch');
+	if (isNotPromise) features.push('Promise');
+	if (isNotAssign) features.push('Object.assign');
+
+	if (isNotPromise || isNotFetch || isNotAssign) {
+		requiredCount++;
+		loader('https://cdn.polyfill.io/v2/polyfill.min.js?features=' + features.join(','), loaded);
 	}
-};
 
-const features = [];
-const isNotFetch = !('fetch' in window);
-const isNotAssign = !('assign' in Object);
-const isNotPromise = !('Promise' in window);
+	if (isNotCustomElement) {
+		requiredCount++;
+		loader('https://cdnjs.cloudflare.com/ajax/libs/document-register-element/1.7.2/document-register-element.js', loaded);
+	}
 
-if (isNotFetch) features.push('fetch');
-if (isNotPromise) features.push('Promise');
-if (isNotAssign) features.push('Object.assign');
-
-loader(
-	isNotPromise || isNotFetch || isNotAssign,
-	'https://cdn.polyfill.io/v2/polyfill.min.js?features=' + features.join(',')
-);
-
-loader(
-	!('registerElement' in document) || !('content' in document.createElement('template')),
-	'https://cdnjs.cloudflare.com/ajax/libs/document-register-element/1.7.2/document-register-element.js'
-);
+	loaded();
+}
 
 class Oxe {
 
 	constructor () {
-		// this.compiled = true;
+		this.compiled = true;
 	}
 
 	get window () {
@@ -209,7 +201,7 @@ class Oxe {
 		return Model;
 	}
 
-	setup (data) {
+	async setup (data) {
 
 		if (this._setup) {
 			return;
