2ae5c0996d8dfe08d0caf48746deca573b39ec99
Alexander Elias <alex.steven.elias@gmail.com>
2019-01-03 16:23:16 -0700

    fixed old option

--- src/listener/state.js
+++ src/listener/state.js
@@ -2,9 +2,9 @@ import Router from '../router.js';
 
 export default function (event) {
 
-	var path = event && event.state ? event.state.path : window.location.href;
+	const path = event && event.state ? event.state.path : window.location.href;
 
 	Promise.resolve().then(function () {
-		return Router.route(path, { replace: true });
+		return Router.route(path, { mode: 'replace' });
 	}).catch(console.error);
 };
