e8dc5db34d8ac7cff101c4c55f9844b4faf5edb0
Alexander Elias <alex.steven.elias@gmail.com>
2019-01-06 22:01:06 -0700

    moved piper back to binder

--- src/binder.js
+++ src/binder.js
@@ -126,6 +126,32 @@ export default {
 
 	},
 
+	piper (binder, data) {
+
+		if (!binder.pipes.length) {
+			return data;
+		}
+
+		let methods = Methods.get(binder.scope);
+
+		if (!methods) {
+			return data;
+		}
+
+		for (let i = 0, l = binder.pipes.length; i < l; i++) {
+			let method = binder.pipes[i];
+
+			if (method in methods) {
+				data = methods[method].call(binder.container, data);
+			} else {
+				throw new Error(`Oxe - pipe method ${method} not found in scope ${binder.scope}`);
+			}
+
+		}
+
+		return data;
+	},
+
 	each (path, callback) {
 		let paths = typeof path === 'string' ? path.split('.') : path;
 		let scope = paths[0];
@@ -154,7 +180,7 @@ export default {
 
 		}
 
-	},	
+	},
 
 	skipChildren (element) {
 
