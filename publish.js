19fe125cc331166e54fd6c71dd737df5ea30330e
Alexander Elias <alex.steven.elias@gmail.com>
2019-01-07 15:11:20 -0700

    4.0.2

--- publish.js
+++ publish.js
@@ -1,18 +1,29 @@
 const Package = require('./package.json');
+const Util = require('util');
 const ChildProcess = require('child_process');
 
-const command = [
-	'node build.js',
-	'rm -r ./docs/*',
-	'muleify -p ./web ./docs',
-	'git add .',
-	`git commit -m "${Package.version}"`,
-	'git push',
-	'npm publish'
-].join(' && ');
+const Exec = Util.promisify(ChildProcess.exec);
 
-ChildProcess.exec(command, function (error, stdout, stderr) {
-	if (error) console.error(error);
-	if (stdout) console.log(stdout);
-	if (stderr) console.warn(stderr);
-});
+(async function() {
+
+	const commands = [
+		'node build.js',
+		'rm -r ./docs/*',
+		'muleify -p ./web ./docs',
+		'git add .',
+		`git commit -m "${Package.version}"`,
+		'git push',
+		'npm publish'
+	];
+
+	for (const command of commands) {
+
+		const { error, stdout, stderr } = await Exec(command);
+
+		if (error) console.error(error);
+		if (stdout) console.log(stdout);
+		if (stderr) console.warn(stderr);
+
+	}
+
+}()).catch(console.error);
