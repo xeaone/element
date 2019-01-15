bb02ed28a13bcd2a592d5cdc3f63c6315bbc3683
Alexander Elias <alex.steven.elias@gmail.com>
2019-01-15 16:30:38 -0700

    major bug fixes, big rewrite

--- src/path.js
+++ src/path.js
@@ -1,73 +1,169 @@
 
+let BASE = window.PATH_BASE || null;
+
 export default {
 
-	extension (data) {
-		let position = data.lastIndexOf('.');
-		return position > 0 ? data.slice(position + 1) : '';
+	get base () {
+		if (BASE) return BASE;
+		const base = window.document.querySelector('base');
+		if (base) return base.href;
+		return window.location.origin + (window.location.pathname ? window.location.pathname : '/');
 	},
 
-	join () {
-		return Array.prototype.join
-			.call(arguments, '/')
-			.replace(/\/{2,}/g, '/')
-			.replace(/^(https?:\/)/, '$1/');
-	},
+	async setup (option) {
+		option = option || {};
 
-	base (href) {
-		let base = window.document.querySelector('base');
+		if (option.base) {
+			let base = window.document.querySelector('base');
 
-		if (href) {
-
-			if (base) {
-				base.href = href;
-			} else {
+			if (!base) {
 				base = window.document.createElement('base');
-				base.href = href;
 				window.document.head.insertBefore(base, window.document.head.firstElementChild);
 			}
 
+			base.href = option.base;
+			BASE = BASE || base.href;
 		}
 
-		return base ? base.href : window.location.origin + window.location.pathname;
 	},
 
-	resolve (path, base) {
-		path = path.replace(window.location.origin, '');
+	extension (data) {
+		const position = data.lastIndexOf('.');
+		return position > 0 ? data.slice(position + 1) : '';
+	},
 
-		if (path.indexOf('http://') === 0 || path.indexOf('https://') === 0 || path.indexOf('file://') === 0 || path.indexOf('//') === 0) {
-			return path;
+	clean (data) {
+
+		if (!data) {
+			throw new Error('Oxe.path.clean - argument required');
 		}
 
-		if (path.charAt(0) !== '/') {
-			base = base || this.base();
-			path = `${base}/${path}`;
-			path = path.replace(window.location.origin, '');
+		const origin = window.location.origin;
+		const protocol = window.location.protocol + '//';
+
+		if (data.slice(0, origin.length) === origin) {
+			data = data.slice(origin.length);
 		}
 
-		path = path.replace(/\/{2,}/, '/');
-		path = path.replace(/^\//, '');
-		path = path.replace(/\/$/, '');
+		if (data.slice(0, protocol.length) === protocol) {
+			data = data.slice(protocol.length);
+		}
 
-		let result = [];
-		let paths = path.split('/');
+		if (data.slice(-window.location.hash.length) === window.location.hash) {
+			data = data.slice(0, -window.location.hash.length);
+		}
 
-		for (let i = 0, l = paths.length; i < l; i++) {
+		if (data.slice(-window.location.search.length) === window.location.search) {
+			data = data.slice(0, -window.location.search.length);
+		}
 
-			if (paths[i] === '.' || paths[i] === '') {
+		return data || '/';
+	},
+
+	normalize (data) {
+
+		if (!data) {
+			throw new Error('Oxe.path.normalize - argument required');
+		}
+
+		let flag = false;
+		const result = [];
+		const parts = this.clean(data).split(/\/+/);
+
+		for (let i = 0, l = parts.length; i < l; i++) {
+			if (parts[i] === '.') {
 				continue;
-			} else if (paths[i] === '..') {
-
-				if (i > 0) {
-					result.splice(i - 1, 1);
+			} else if (parts[i] === '') {
+				if (i === 0) {
+					flag = true;
 				}
-
+			} else if (parts[i] === '..') {
+				if (i > 0) {
+					result.pop();
+				}
 			} else {
-				result.push(paths[i]);
+				result.push(parts[i]);
+			}
+		}
+
+		return (flag ? '/' : '') + result.join('/');
+	},
+
+	join () {
+
+		if (!arguments.length) {
+			throw new Error('Oxe.path.join - argument required');
+		}
+
+		const result = [];
+
+		for (let i = 0, l = arguments.length; i < l; i++) {
+			if (arguments[i]) {
+				result.push(this.clean(arguments[i]));
+			}
+		}
+
+		return this.normalize(result.join('/'));
+	},
+
+	resolve () {
+		console.log(arguments);
+
+		if (!arguments.length) {
+			throw new Error('Oxe.path.resolve - argument required');
+		}
+
+		const result = [];
+
+		let argument;
+		let i = arguments.length;
+
+		while (argument = arguments[--i]) {
+			result.unshift(this.clean(argument));
+			// result.unshift(this.normalize(argument));
+
+			if (argument[0] === '/') {
+				break;
 			}
 
 		}
 
-		return '/' + result.join('/');
+		// let path = result.join('/');
+
+		// console.log('Path.resolve.in:', result.join('/'));
+
+		// path = path.replace(window.location.hash, '');
+		// path = path.replace(window.location.search, '');
+		// path = path.replace(window.location.origin, '');
+		// path = path.replace(window.location.protocol, '');
+
+		// if (path.indexOf('http://') === 0 || path.indexOf('https://') === 0 || path.indexOf('file://') === 0 || path.indexOf('//') === 0) {
+		// 	return path;
+		// }
+
+		// base = base || this.base || '';
+
+		console.log(result);
+
+		if (result[0][0] !== '/') {
+			// let base = this.base || '';
+			// base = base || this.base || '';
+			let base = this.base || window.location.pathname ? window.location.pathname : '/';
+			// path = `${base}/${path}`;
+			result.unshift(this.base);
+		}
+
+		console.log(result);
+
+		// path = `${base}/${path}`;
+		// path = path.replace(window.location.origin, '');
+		// path = path.replace(window.location.protocol, '');
+		// path = path.replace(/^\//, '');
+		// path = path.replace(/\/$/, '');
+
+		// console.log('Path.resolve.out:', this.normalize(result.join('/')));
+
+		return this.normalize(result.join('/'));
 	}
 
 }
