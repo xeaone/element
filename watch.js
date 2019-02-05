ea109cda591788660be07a38b73246d66cfa759c
Alexander Elias <alex.steven.elias@gmail.com>
2019-02-05 16:20:11 -0700

    fixed missing compile on start, added more logs

--- watch.js
+++ watch.js
@@ -25,47 +25,54 @@ const Watcher = async function (data, listener) {
 
 };
 
-(async function () {
+const compile = async function () {
+	const bundled = await Rollup.rollup({ input: 'src/index.js' });
 
-	await Watcher('./src', async function () {
-		const bundled = await Rollup.rollup({ input: 'src/index.js' });
-
-		const generated = await bundled.generate({
-			name: 'Oxe',
-			indent: '\t',
-			// format: 'esm',
-			format: 'umd',
-			treeshake: true
-		});
-
-		const code = generated.output[0].code;
-
-		const options = {
-			moduleId: 'Oxe',
-			comments: false,
-			sourceMaps: false,
-			plugins: [
-				['module:fast-async', {
-					spec: true
-				}]
-	        ],
-			presets: [
-				['@babel/preset-env', {
-					modules: false,
-					// useBuiltIns: 'usage',
-					targets: { ie: '11' },
-					exclude: [
-						'transform-regenerator',
-						'transform-async-to-generator',
-						'proposal-async-generator-functions',
-					]
-				}]
-			]
-		};
-
-		const dev = Babel.transform(code, options);
-		await WriteFile('web/assets/oxe.js', dev.code)
-
+	const generated = await bundled.generate({
+		name: 'Oxe',
+		indent: '\t',
+		// format: 'esm',
+		format: 'umd',
+		treeshake: true
 	});
+
+	const code = generated.output[0].code;
+
+	const options = {
+		moduleId: 'Oxe',
+		comments: false,
+		sourceMaps: false,
+		plugins: [
+			['module:fast-async', {
+				spec: true
+			}]
+        ],
+		presets: [
+			['@babel/preset-env', {
+				modules: false,
+				// useBuiltIns: 'usage',
+				targets: { ie: '11' },
+				exclude: [
+					'transform-regenerator',
+					'transform-async-to-generator',
+					'proposal-async-generator-functions',
+				]
+			}]
+		]
+	};
+
+	const dev = Babel.transform(code, options);
+
+	await WriteFile('web/assets/oxe.js', dev.code);
+
+	console.log('\nWatch Compile Ended');
+};
+
+(async function () {
+	console.log('\nWatch Compile Started');
+
+	await compile();
+
+	await Watcher('./src', compile);
 
 }()).catch(console.error);
