5bbc44bd92b163eb0bd85cedfe90fc4e455d2e8f
Alexander Elias <alex.steven.elias@gmail.com>
2019-02-05 17:05:47 -0700

    better watching and key recompile

--- watch.js
+++ watch.js
@@ -5,32 +5,37 @@ const Babel = require('@babel/core');
 const Fs = require('fs');
 const Util = require('util');
 const Path = require('path');
+const Readline = require('readline');
 
 const ReadFolder = Util.promisify(Fs.readdir);
 const WriteFile = Util.promisify(Fs.writeFile);
 
+let Busy = false;
+
+const Keypresser = async function (listener) {
+	Readline.emitKeypressEvents(process.stdin);
+
+	process.stdin.setRawMode(true);
+
+	process.stdin.on('keypress', async function (key, data) {
+		if (data.ctrl && data.name === 'c') {
+			process.exit();
+		} else {
+			Promise.resolve(listener(key)).catch(console.error);
+		}
+	});
+};
+
 const Watcher = async function (data, listener) {
 	const paths = await ReadFolder(data);
-
-	let busy = false;
 
 	for (const path of paths) {
 		const item = Path.resolve(Path.join(data, path));
 
-		const callback = function (type, name) {
-			if (busy === false) {
-				Promise.resolve().then(function () {
-					busy = true;
-				}).then(function () {
-					return listener(type, name);
-				}).then(function () {
-					busy = false;
-				}).catch(console.error);
-			}
-		};
-
 		if (item.includes('.')) {
-			Fs.watch(item, callback);
+			Fs.watch(item, function (type, name) {
+				Promise.resolve(listener(type, name)).catch(console.error);
+			});
 		} else {
 			await Watcher(item, listener);
 		}
@@ -40,6 +45,9 @@ const Watcher = async function (data, listener) {
 };
 
 const compile = async function () {
+	if (Busy) return;
+	else Busy = true;
+
 	console.log('\nWatch Compile Start\n');
 
 	const bundled = await Rollup.rollup({ input: 'src/index.js' });
@@ -82,11 +90,19 @@ const compile = async function () {
 	await WriteFile('web/assets/oxe.js', dev.code);
 
 	console.log('\nWatch Compile End\n');
+
+	Busy = false;
 };
 
 (async function () {
 
 	await compile();
+
+	await Keypresser(async function (key) {
+		if (key === 'c') {
+			await compile();
+		}
+	});
 
 	await Watcher('./src', compile);
 
