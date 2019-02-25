53319ccbd2399e4649f43ad08de9373f21543301
Alexander Elias <alex.steven.elias@gmail.com>
2019-02-25 16:31:07 -0700

    code cleanup

--- web/performance/index.js
+++ web/performance/index.js
@@ -10,10 +10,8 @@ var oLoop = {
 		console.log(this.model);
 	},
 	methods: {
-		loop: function () {
+		push: function () {
 			console.time('push');
-
-			var time = performance.now();
 
 			for (var i = 0; i < this.model.count; i++) {
 				this.model.items.push({ number: this.model.items.length });
@@ -21,42 +19,33 @@ var oLoop = {
 
 			console.timeEnd('push');
 		},
-		over: function () {
-			const self = this;
+		overwrite: function () {
+			console.time('overwrite');
 
-			console.log(self.model.items);
-			console.log(self.model.items.length);
-			console.time('over');
-
-			var time = performance.now();
 			var items = [];
 
 			for (var i = 0; i < 10; i++) {
 				items.push({ number: i });
 			}
 
-			self.model.items = items;
+			this.model.items = items;
 
-			console.timeEnd('over')
-			console.log(self.model.items);
-			console.log(self.model.items.length);
-
-			setTimeout(function () {
-				console.log(self.model.items.length);
-			}, 3000);
+			console.timeEnd('overwrite')
 		}
 	},
-	template: `
+	template: /*html*/`
 
 		<h3><span o-text="count"></span> Inputs two way binded</h3>
 		<h3><span o-text="count"></span> Text one way binded</h3>
 
-		<form o-submit="loop">
+		<form o-submit="push">
 			<input o-value="count" type="number">
-			<input type="submit" value="Loop">
+			<input type="submit" value="Push">
 		</form>
 
-		<button o-on-click="over">Over</button>
+		<br>
+
+		<button o-on-click="overwrite">Overwrite</button>
 
 		<div o-each-item="items">
 			<div class="box">
@@ -65,8 +54,6 @@ var oLoop = {
 			</div>
 		</div>
 
-		<!--
-		-->
 	`,
 };
 
