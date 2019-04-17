bd3b24d7c60dd1ad7b8a4a61559edca140434e1d
Alexander Elias <alex.steven.elias@gmail.com>
2019-04-17 12:05:28 -0700

    bug fixes

--- src/binders/value.js
+++ src/binders/value.js
@@ -1,132 +1,130 @@
 import Utility from '../utility.js';
-import Piper from '../piper.js';
 
 export default function (binder) {
-	const self = this;
-	const type = binder.target.type;
-	const name = binder.target.nodeName;
+    const type = binder.target.type;
+    const name = binder.target.nodeName;
 
-	if (name === 'SELECT' || name.indexOf('-SELECT') !== -1) {
-		return {
-			read () {
-				this.data = binder.data;
+    if (name === 'SELECT' || name.indexOf('-SELECT') !== -1) {
+        return {
+            read () {
+                this.data = binder.data;
 
-				this.nodes = binder.target.options;
-				this.multiple = Utility.multiple(binder.target);
+                this.nodes = binder.target.options;
+                this.multiple = Utility.multiple(binder.target);
 
-				if (this.multiple && (!this.data || this.data.constructor !== Array)) {
-					throw new Error(`Oxe - invalid o-value ${binder.keys.join('.')} multiple select requires array`);
-				}
+                if (this.multiple && (!this.data || this.data.constructor !== Array)) {
+                    throw new Error(`Oxe - invalid o-value ${binder.keys.join('.')} multiple select requires array`);
+                }
 
-			},
-			write () {
-				for (let i = 0, l = this.nodes.length; i < l; i++) {
-					const node = this.nodes[i];
-					const value = Utility.value(node, binder.container.model);
+            },
+            write () {
+                for (let i = 0, l = this.nodes.length; i < l; i++) {
+                    const node = this.nodes[i];
+                    const value = Utility.value(node, binder.container.model);
 
-					if (this.multiple) {
-						if (node.selected) {
+                    if (this.multiple) {
+                        if (node.selected) {
 
-							if (!node.disabled && !Utility.includes(this.data, value)) {
-								binder.data.push(value);
-							}
+                            if (!node.disabled && !Utility.includes(this.data, value)) {
+                                binder.data.push(value);
+                            }
 
-						} else if (Utility.includes(this.data, value)) {
-							node.selected = true;
-						}
-					} else {
-						if (node.selected) {
+                        } else if (this.data !== undefined && Utility.includes(this.data, value)) {
+                            node.selected = true;
+                        }
+                    } else {
+                        if (node.selected) {
 
-							if (!node.disabled && !Utility.compare(this.data, value)) {
-								binder.data = value;
-							}
+                            if (!node.disabled && !Utility.compare(this.data, value)) {
+                                binder.data = value;
+                            }
 
-							break;
-						} else if (Utility.compare(this.data, value)) {
-							node.selected = true;
-							break;
-						}
-					}
-				}
-			}
-		};
-	} else if (type === 'radio') {
-		return {
-			read () {
-				this.data = binder.data;
+                            break;
+                        } else if (this.data !== undefined && Utility.compare(this.data, value)) {
+                            node.selected = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        };
+    } else if (type === 'radio') {
+        return {
+            read () {
+                this.data = binder.data;
 
-				if (typeof this.data !== 'number') {
-					// this.data = 0;
-					return false;
-				}
+                if (typeof this.data !== 'number') {
+                    // this.data = 0;
+                    return false;
+                }
 
-				this.nodes = binder.container.querySelectorAll(
-					'input[type="radio"][o-value="' + binder.value + '"]'
-				);
+                this.nodes = binder.container.querySelectorAll(
+                    'input[type="radio"][o-value="' + binder.value + '"]'
+                );
 
-			},
-			write () {
-				let checked = false;
+            },
+            write () {
+                let checked = false;
 
-				for (let i = 0, l = this.nodes.length; i < l; i++) {
-					const node = this.nodes[i];
+                for (let i = 0, l = this.nodes.length; i < l; i++) {
+                    const node = this.nodes[i];
 
-					if (i === this.data) {
-						checked = true;
-						node.checked = true;
-						node.setAttribute('checked', '');
-					} else {
-						node.checked = false;
-						node.removeAttribute('checked');
-					}
+                    if (i === this.data) {
+                        checked = true;
+                        node.checked = true;
+                        node.setAttribute('checked', '');
+                    } else {
+                        node.checked = false;
+                        node.removeAttribute('checked');
+                    }
 
-				}
+                }
 
-				if (!checked) {
-					this.nodes[0].checked = true;
-					this.nodes[0].setAttribute('checked', '');
-					// binder.data = 0;
-				}
+                if (!checked) {
+                    this.nodes[0].checked = true;
+                    this.nodes[0].setAttribute('checked', '');
+                    // binder.data = 0;
+                }
 
-			}
-		};
+            }
+        };
 
-	} else if (type === 'checkbox' || name.indexOf('-CHECKBOX') !== -1) {
-		return {
-			read () {
-				this.data = binder.data;
+    } else if (type === 'checkbox' || name.indexOf('-CHECKBOX') !== -1) {
+        return {
+            read () {
+                this.data = binder.data;
 
-				if (typeof this.data !== 'boolean') {
-					// this.data = false;
-					return false;
-				}
+                if (typeof this.data !== 'boolean') {
+                    // this.data = false;
+                    return false;
+                }
 
-			},
-			write () {
+            },
+            write () {
 
-				binder.target.checked = this.data;
+                binder.target.checked = this.data;
 
-				if (this.data) {
-					binder.target.setAttribute('checked', '');
-				} else {
-					binder.target.removeAttribute('checked');
-				}
+                if (this.data) {
+                    binder.target.setAttribute('checked', '');
+                } else {
+                    binder.target.removeAttribute('checked');
+                }
 
-			}
-		};
-	} else {
-		return {
-			read () {
-				this.data = binder.data;
+            }
+        };
+    } else {
+        return {
+            read () {
+                this.data = binder.data;
 
-				if (this.data === binder.target.value) {
-					return false;
-				}
+                if (this.data === binder.target.value) {
+                    return false;
+                }
 
-			},
-			write () {
-				binder.target.value = this.data === undefined || this.data === null ? '' : this.data;
-			}
-		};
-	}
-};
+            },
+            write () {
+                binder.target.value = this.data === undefined || this.data === null ? '' : this.data;
+            }
+        };
+    }
+}
--- src/update.js
+++ src/update.js
@@ -1,66 +1,38 @@
 import Batcher from './batcher.js';
 import Utility from './utility.js';
-import Model from './model.js';
 import Binder from './binder.js';
-// import View from './view.js';
 
 export default async function (node, attribute) {
 
-	if (!node) throw new Error('Oxe.update - requires node argument');
-	if (!attribute) throw new Error('Oxe.update - requires attribute argument');
+    if (!node) throw new Error('Oxe.update - requires node argument');
+    if (!attribute) throw new Error('Oxe.update - requires attribute argument');
 
-	const binder = Binder.get('attribute', node, attribute);
+    const binder = Binder.get('attribute', node, attribute);
 
-	const read = function () {
-		const type = binder.target.type;
-	 	const name = binder.target.nodeName;
-		// let data = Utility.value(binder.target, binder.container.model);
+    const read = function () {
+        const type = binder.target.type;
+        const name = binder.target.nodeName;
 
-		if (name === 'SELECT' || name.indexOf('-SELECT') !== -1) {
-			const value = Utility.value(binder.target, binder.container.model);
-			binder.data = value;
-			// const nodes = binder.target.options;
-			// const multiple = Utility.multiple(binder.target);
-			//
-		 	// let result = multiple ? [] : undefined;
-			//
-			// for (let i = 0, l = nodes.length; i < l; i++) {
-			// 	const node = nodes[i];
-			//
-			// 	if (!Utility.selected(node)) continue;
-			//
-			// 	const value = Utility.value(node, binder.container.model);
-			//
-			// 	if (multiple) {
-			// 		result.push(value);
-			// 	} else {
-			// 		result = value;
-			// 		break;
-			// 	}
-			//
-			// }
-			//
-			// binder.data = result;
-		} else if (type === 'radio') {
-			const query = 'input[type="radio"][o-value="' + binder.value + '"]';
-			const nodes = binder.container.querySelectorAll(query);
+        if (name === 'SELECT' || name.indexOf('-SELECT') !== -1) {
+            const value = Utility.value(binder.target, binder.container.model);
+            binder.data = value;
+        } else if (type === 'radio') {
+            const query = 'input[type="radio"][o-value="' + binder.value + '"]';
+            const nodes = binder.container.querySelectorAll(query);
 
-			for (let i = 0, l = nodes.length; i < l; i++) {
-				const node = nodes[i];
+            for (let i = 0, l = nodes.length; i < l; i++) {
+                if (binder.target === nodes[i]) {
+                    binder.data = i;
+                }
 
-				if (binder.target === node) {
-					console.log(i);
-					// data = i;
-				}
+            }
 
-			}
+        } else if (type === 'checkbox' || name.indexOf('-CHECKBOX') !== -1) {
+            binder.data = binder.target.checked || false;
+        } else {
+            binder.data = binder.target.value || '';
+        }
+    };
 
-		} else if (type === 'checkbox' || name.indexOf('-CHECKBOX') !== -1) {
-			binder.data = binder.target.checked || false;
-		} else {
-			binder.data = binder.target.value || '';
-		}
-	};
-
-	Batcher.batch({ read });
-};
+    Batcher.batch({ read });
+}
