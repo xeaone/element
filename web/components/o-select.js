122455bc78301cdaf120f6ae3a394ac14c9e4d72
Alexander Elias <alex.steven.elias@gmail.com>
2019-12-04 12:19:12 -0800

    updated o-select

--- web/components/o-select.js
+++ web/components/o-select.js
@@ -1,9 +1,119 @@
+/*
+	Name: oxe-components
+	Version: 4.1.1
+	License: MPL-2.0
+	Author: Arc IO
+	Email: undefined
+	This Source Code Form is subject to the terms of the Mozilla Public
+	License, v. 2.0. If a copy of the MPL was not distributed with this
+	file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
 
 export default [
     {
-        name: 'o-optgroup',
-        style: 'o-optgroup { display: block; } o-optgroup::before { content: attr(label); }',
+        name: 'o-select',
         template: '<slot></slot>',
+        attributes: [ 'multiple' ],
+        style: 'o-select { display: block; }',
+        created: function () {
+            this.tabIndex = 0;
+        },
+        attributed: function (name, _, data) {
+            switch (name) {
+                case 'multiple': this.multiple = false; break;
+            }
+        },
+        properties: {
+            _selectedOptions: { writable: true, value: [] },
+            selectedOptions: {
+                enumerable: true,
+                get: function () {
+                    return this._selectedOptions;
+                }
+            },
+            required: {
+                enumerable: true,
+                get: function () {
+                    return this.hasAttribute('required');
+                },
+                set: function (data) {
+                    data = data ? true : false;
+                    if (data) this.setAttribute('required', '');
+                    else this.removeAttribute('required');
+                    return data;
+                }
+            },
+            checkValidity: {
+                enumerable: true,
+                value: function () {
+                    if (this.required) {
+                        return this._selectedOptions.length ? true : false;
+                    } else {
+                        return true;
+                    }
+                }
+            },
+            type: {
+                enumerable: true,
+                get: function () {
+                    return this.hasAttribute('multiple') ? 'select-multiple' : 'select-one';
+                }
+            },
+            options: {
+                enumerable: true,
+                get: function () {
+                    return this.querySelectorAll('o-option');
+                }
+            },
+            disabled: {
+                enumerable: true,
+                get: function () {
+                    return this.hasAttribute('disabled');
+                },
+                set: function (data) {
+                    data = data ? true : false;
+                    if (data) this.setAttribute('disabled', '');
+                    else this.removeAttribute('disabled');
+                    return data;
+                }
+            },
+            multiple: {
+                enumerable: true,
+                get: function () {
+                    return this.hasAttribute('multiple');
+                },
+                set: function (data) {
+                    data = data ? true : false;
+
+                    if (data) {
+                        this.setAttribute('multiple', '');
+                    } else {
+                        this.removeAttribute('multiple');
+                        this._selectedOptions.slice(1).forEach(function (option) {
+                            option.selected = false;
+                        });
+                    }
+
+                    return data;
+                }
+            },
+            name: {
+                enumerable: true,
+                get: function () {
+                    return this.getAttribute('name') || '';
+                },
+                set: function (data) {
+                    this.setAttribute('name', data);
+                    return data;
+                }
+            }
+        }
+    },
+    {
+        name: 'o-optgroup',
+        attributes: ['label'],
+        template: '<slot></slot>',
+        style: 'o-optgroup { display: block; } o-optgroup::before { content: attr(label); }',
         properties: {
             disabled: {
                 enumerable: true,
@@ -16,6 +126,25 @@ export default [
                     else this.removeAttribute('disabled');
                     return data;
                 }
+            },
+            label: {
+                enumerable: true,
+                get: function () {
+                    return this.getAttribute('label');
+                },
+                set: function (data) {
+                    return this.setAttribute('label', data);
+                }
+            }
+        },
+        attributed: function (name, _, data) {
+            switch (name) {
+                case 'label': this.label = data; break;
+            }
+        },
+        created: function () {
+            if (this.parentElement && this.parentElement.nodeName !== 'O-SELECT') {
+                console.warn('o-optgroup invalid parent element');
             }
         }
     },
@@ -25,6 +154,28 @@ export default [
         style: 'o-option { display: block; }',
         attributes: [ 'value' ],
         properties: {
+            _select: {
+                get: function () {
+                    if (!this.parentElement) {
+                        return null;
+                    } else if (this.parentElement.nodeName === 'O-SELECT') {
+                        return this.parentElement;
+                    } else if (!this.parentElement.parentElement) {
+                        return null;
+                    } else if (this.parentElement.parentElement.nodeName === 'O-SELECT') {
+                        return this.parentElement.parentElement;
+                    } else {
+                        console.warn('o-option invalid parent type');
+                    }
+                }
+            },
+            _group: {
+                get: function () {
+                    if (this.parentElement && this.parentElement.nodeName === 'O-OPTGROUP') {
+                        return this.parentElement;
+                    }
+                }
+            },
             _valueDefaultLocked: {
                 writable: true,
                 value: false
@@ -68,11 +219,34 @@ export default [
                 },
                 set: function (data) {
                     this._selectedDefaultLocked = true;
-                    this._selected = data ? true : false;
-                    // maybe use toggle attribute neeed to test in IE
-                    if (this._selected) this.setAttribute('data-selected', '');
+
+                    var selected = this._selected = data ? true : false;
+                    if (selected) this.setAttribute('data-selected', '');
                     else this.removeAttribute('data-selected');
-                    return this._selected;
+
+                    var select = this._select;
+                    if (!select) return selected;
+
+                    if (select.multiple === false) {
+                        var old = select._selectedOptions[0];
+                        if (old && this !== old) {
+                            old.selected = false
+                        }
+                    }
+
+                    var index = select._selectedOptions.indexOf(this);
+
+                    if (selected) {
+                        if (index === -1) {
+                            select._selectedOptions.push(this);
+                        }
+                    } else {
+                        if (index !== -1) {
+                           select._selectedOptions.splice(index, 1);
+                        }
+                    }
+
+                    return selected;
                 }
             },
             disabled: {
@@ -96,176 +270,39 @@ export default [
                     this.setAttribute('name', data);
                     return data;
                 }
-            }
+            },
         },
         attributed: function (name, _, data) {
             switch (name) {
-            case 'value': this._value = data || ''; break;
-            }
-        },
-        created: function () {
-            if (this.hasAttribute('selected')) {
-                this.setAttribute('data-selected', '');
-            } else {
-                this.removeAttribute('data-selected');
-            }
-        }
-    },
-    {
-        name: 'o-select',
-        template: '<slot></slot>',
-        style: 'o-select { display: block; }',
-        properties: {
-            _selectedOptions: { writable: true, value: [] },
-            // _selectedIndex: { writable: true, value: -1 },
-            // selectedIndex: {
-            //     enumerable: true,
-            //     get: function () {
-            //         return this._selectedIndex;
-            //     }
-            // },
-            selectedOptions: {
-                enumerable: true,
-                get: function () {
-                    return this._selectedOptions;
-                }
-            },
-            required: {
-                enumerable: true,
-                get: function () {
-                    return this.hasAttribute('required');
-                },
-                set: function (data) {
-                    data = data ? true : false;
-                    if (data) this.setAttribute('required', '');
-                    else this.removeAttribute('required');
-                    return data;
-                }
-            },
-            checkValidity: {
-                enumerable: true,
-                value: function () {
-                    if (this.required) {
-                        return this.selectedOptions.length ? true : false;
-                    } else {
-                        return true;
-                    }
-                }
-            },
-            type: {
-                enumerable: true,
-                get: function () {
-                    return this.hasAttribute('multiple') ? 'select-multiple' : 'select-one';
-                }
-            },
-            options: {
-                enumerable: true,
-                get: function () {
-                    return this.querySelectorAll('o-option');
-                }
-            },
-            disabled: {
-                enumerable: true,
-                get: function () {
-                    return this.hasAttribute('disabled');
-                },
-                set: function (data) {
-                    data = data ? true : false;
-                    if (data) this.setAttribute('disabled', '');
-                    else this.removeAttribute('disabled');
-                    return data;
-                }
-            },
-            multiple: {
-                enumerable: true,
-                get: function () {
-                    return this.hasAttribute('multiple');
-                },
-                set: function (data) {
-                    data = data ? true : false;
-                    if (data) this.setAttribute('multiple', '');
-                    else this.removeAttribute('multiple');
-                    return data;
-                }
-            },
-            name: {
-                enumerable: true,
-                get: function () {
-                    return this.getAttribute('name') || '';
-                },
-                set: function (data) {
-                    this.setAttribute('name', data);
-                    return data;
-                }
+                case 'value': this._value = data || ''; break;
             }
         },
         created: function () {
             var self = this;
 
-            self.addEventListener('click', function (e) {
+            self.addEventListener('click', function () {
+                var group = self._group;
+                var select = self._select;
 
-                if (self.disabled || e.target === self) {
+                if (self.disabled || (select && select.disabled) || (group && group.disabled)) {
                     return;
                 }
 
-                var option = e.target;
+                self.selected = !self.selected;
 
-                if (option.nodeName !== 'O-OPTION') {
-                    while (option = option.parentElement) {
-                        if (option === self) {
-                            return;
-                        } else if (option.nodeName === 'O-OPTION') {
-                            if (option.disabled) {
-                                return;
-                            } else {
-                                break;
-                            }
-                        }
-                    }
+                if (select) {
+                    var binder = Oxe.binder.get(select, 'o-value');
+                    if (binder) Oxe.binder.render(binder, 'view');
                 }
 
-                var optgroup = option;
-
-                if (optgroup && optgroup.nodeName !== 'O-OPTGROUP') {
-                    while (optgroup = optgroup.parentElement) {
-                        if (optgroup === self) {
-                            break;
-                        } else if (optgroup.nodeName === 'O-OPTGROUP') {
-                            if (optgroup.disabled) {
-                                return;
-                            } else {
-                                break;
-                            }
-                        }
-                    }
-                }
-
-                if (!self.multiple) {
-                    var options = self.options;
-                    for (var i = 0, l = options.length; i < l; i++) {
-                        options[i].selected = false;
-                    }
-                }
-
-                if (option) {
-                    option.selected = !option.selected;
-                }
-
-                var index = self._selectedOptions.indexOf(option);
-
-                if (option.selected) {
-                    if (index === -1) {
-                        self._selectedOptions.push(option);
-                    }
-                } else {
-                    if (index !== -1) {
-                        self._selectedOptions.splice(index, 1);
-                    }
-                }
-
-                var binder = Oxe.binder.get('attribute', self, 'o-value');
-                Oxe.binder.render(binder, 'view');
             });
+
+            if (self.hasAttribute('selected')) {
+                click();
+                self.setAttribute('data-selected', '');
+            } else {
+                self.removeAttribute('data-selected');
+            }
 
         }
     }
