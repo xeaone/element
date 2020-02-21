65f07bd0fb992b4439a78effbed93c11efcb9a87
Alexander Elias <alex.steven.elias@gmail.com>
2020-02-20 18:40:03 -0800

    removed old babel plugins

--- packer.mjs
+++ packer.mjs
@@ -35,10 +35,6 @@ export const Transformer = async function (option) {
         minified: option.minify,
         comments: option.comments,
         plugins: [
-            // '@babel/plugin-transform-spread',
-            // '@babel/plugin-syntax-dynamic-import',
-            // '@babel/plugin-transform-destructuring',
-            // '@babel/plugin-transform-arrow-functions',
             'babel-plugin-transform-async-to-promises'
         ],
         presets: [
