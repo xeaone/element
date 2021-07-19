b569b3c43a425cf92c1fa968a7f9c3ab949070de
Alexander Elias <alex.steven.elias@gmail.com>
2021-07-19 11:23:06 -0700

    proxy fixes

--- src/observer.ts
+++ src/observer.ts
@@ -7,6 +7,7 @@ const $path = Symbol('$path');
 const $task = Symbol('$task');
 const $tasks = Symbol('$tasks');
 const $proxy = Symbol('$proxy');
+const $setup = Symbol('$setup');
 
 // const tick = Promise.resolve();
 
@@ -22,26 +23,27 @@ const set = function (target: any, key: any, value: any, receiver: any) {
     if (key === $task) return true;
     if (key === $tasks) return true;
     if (key === $proxy) return true;
+    if (key === $setup) return true;
     if (key === 'length') return target[ $tasks ].push(target[ $task ].bind(null, target[ $path ]));
 
     const current = target[ key ];
-    if (current !== current && value !== value) return true;
-    if (current === value) return true;
+    if (current !== current && value !== value) return true; // NaN check
+    if (current === value && target[ $setup ]) return true;
 
     const path = target[ $path ] ? `${target[ $path ]}.${key}` : `${key}`;
     const initial = !target[ $tasks ].length;
     target[ $tasks ].push(target[ $task ].bind(null, path));
 
     if (value && typeof value === 'object' && !value[ $proxy ]) {
-
         value[ $path ] = path;
         value[ $proxy ] = true;
+        value[ $setup ] = false;
         value[ $task ] = target[ $task ];
         value[ $tasks ] = target[ $tasks ];
-        target[ key ] = new Proxy(value, handler);
-        // const proxy = new Proxy(value, handler);
-        // target[ key ] = Object.assign(proxy, value);
-
+        const proxy = new Proxy(value, handler);
+        Object.assign(proxy, value);
+        value[ $setup ] = true;
+        target[ key ] = proxy;
     } else {
         target[ key ] = value;
     }
@@ -54,19 +56,14 @@ const set = function (target: any, key: any, value: any, receiver: any) {
 const handler = { set };
 
 const observer = function (source: any, task: task) {
-    // source[ $path ] = '';
-    // source[ $tasks ] = [];
-    // source[ $task ] = task;
-    // source[ $proxy ] = true;
-    // return new Proxy(source, { set });
-
-    const proxy = new Proxy({
-        [ $path ]: '',
-        [ $tasks ]: [],
-        [ $task ]: task,
-        [ $proxy ]: true
-    }, handler);
+    source[ $path ] = '';
+    source[ $tasks ] = [];
+    source[ $task ] = task;
+    source[ $proxy ] = true;
+    source[ $setup ] = false;
+    const proxy = new Proxy(source, handler);
     Object.assign(proxy, source);
+    source[ $setup ] = true;
     return proxy;
 };
 
