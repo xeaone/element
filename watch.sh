20095a33e2eb1522901ac1abeba8021017ef0563
Alexander Elias <alex.steven.elias@gmail.com>
2022-04-05 17:02:15 -0700

    added option

--- watch.sh
+++ watch.sh
@@ -7,7 +7,7 @@ clean() {
     exit
 }
 
-deno bundle --watch src/element/element.ts web/x-element.js & \
+deno bundle --no-clear-screen --watch src/element/element.ts web/x-element.js & \
 deno run --allow-net --allow-read https://deno.land/std/http/file_server.ts ./web -p 8080
 
 return 0
