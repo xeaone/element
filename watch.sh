1749c0ebb9d2ff20ac2c1f58fb9add04721dca54
Alexander Elias <alex.steven.elias@gmail.com>
2022-04-06 17:23:31 -0700

    added router to watch

--- watch.sh
+++ watch.sh
@@ -7,7 +7,8 @@ clean() {
     exit
 }
 
+deno bundle --no-clear-screen --watch src/router/router.ts web/x-router.js & \
 deno bundle --no-clear-screen --watch src/element/element.ts web/x-element.js & \
-deno run --allow-net --allow-read https://deno.land/std/http/file_server.ts ./web -p 8080
+deno run --watch --allow-net --allow-read https://deno.land/std/http/file_server.ts ./web -p 8080
 
 return 0
