40daf04c42599b21ddb4b5d45fa49a98aac35fae
Alexander Elias <alex.steven.elias@gmail.com>
2022-08-12 11:35:01 -0700

    7.3.10

--- web/x-element.js
+++ web/x-element.js
@@ -1,6 +1,6 @@
 /************************************************************************
 Name: XElement
-Version: 7.3.9
+Version: 7.3.10
 License: MPL-2.0
 Author: Alexander Elias
 Email: alex.steven.elis@gmail.com
