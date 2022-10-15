0d2d1672616ed9ab26a70f0d44fbd9027676ecac
Alexander Elias <alex.steven.elias@gmail.com>
2022-10-14 17:45:26 -0700

    code clean up

--- web/guide.js
+++ web/guide.js
@@ -16,8 +16,8 @@ const html = /*html*/`
 <section id="text">
     <h3>Text Binder</h3>
     <template id="text-template">
-    <span text="{{text}}"></span>
-    <span>One: {{text}} Two: {{text}}</span>
+        <span text="{{text}}"></span>
+        <span>One: {{text}} Two: {{text}}</span>
     </template>
     <pre html="{{highlight('#text-template')}}"></pre>
     <article html="{{result('#text-template')}}"></article>
@@ -26,22 +26,22 @@ const html = /*html*/`
 <section id="checked">
 <h3>Checked Binder</h3>
     <template id="checked-template">
-    <input type="checkbox" checked="{{ checked = event ? this.checked : checked }}" value="{{ checked }}" />
-    <span>Checkbox {{ checked ? 'checked' : '' }}</span>
+        <input type="checkbox" checked="{{ checked = event ? this.checked : checked }}" value="{{ checked }}" />
+        <span>Checkbox {{ checked ? 'checked' : '' }}</span>
     </template>
     <pre html="{{highlight('#checked-template')}}"></pre>
     <article html="{{result('#checked-template')}}"></article>
 
     <template id="radio-template">
-    <span>Value: {{radioValue}}</span>
+        <span>Value: {{radioValue}}</span>
 
-    <input type="radio" name="radio" value="{{ radioValue = radioOne ? 'one' : radioValue }}" checked="{{ radioOne = event ? this.checked : radioOne }}" />
-    <span> Radio One </span>
-    <span text="{{radioOne ? 'checked' : ''}}"></span>
+        <input type="radio" name="radio" value="{{ radioValue = radioOne ? 'one' : radioValue }}" checked="{{ radioOne = event ? this.checked : radioOne }}" />
+        <span> Radio One </span>
+        <span text="{{radioOne ? 'checked' : ''}}"></span>
 
-    <input type="radio" name="radio" value="{{ radioValue = radioTwo ? 'two' : radioValue }}" checked="{{ radioTwo = event ? this.checked : radioTwo }}" />
-    <span> Radio Two </span>
-    <span text="{{radioTwo ? 'checked' : ''}}"></span>
+        <input type="radio" name="radio" value="{{ radioValue = radioTwo ? 'two' : radioValue }}" checked="{{ radioTwo = event ? this.checked : radioTwo }}" />
+        <span> Radio Two </span>
+        <span text="{{radioTwo ? 'checked' : ''}}"></span>
     </template>
     <pre html="{{highlight('#radio-template')}}"></pre>
     <article html="{{result('#radio-template')}}"></article>
@@ -50,8 +50,8 @@ const html = /*html*/`
 <section id="style">
     <h3>Style Binder</h3>
     <template id="style-template">
-    <span style="{{ 'color:' + color }}">Look at my style</span>
-    <button onclick="{{colorChange()}}">Change Color</button>
+        <span style="{{ 'color:' + color }}">Look at my style</span>
+        <button onclick="{{colorChange()}}">Change Color</button>
     </template>
     <pre html="{{highlight('#style-template')}}"></pre>
     <article html="{{result('#style-template')}}"></article>
@@ -60,8 +60,8 @@ const html = /*html*/`
 <section id="class">
     <h3>Class Binder</h3>
     <template id="class-template">
-    <span class="{{ active ? 'default class-color' : 'default' }}">Look at my class</span>
-    <button onclick="{{classToggle()}}">Toggle Active</button>
+        <span class="{{ active ? 'default class-color' : 'default' }}">Look at my class</span>
+        <button onclick="{{classToggle()}}">Toggle Active</button>
     </template>
     <pre html="{{highlight('#class-template')}}"></pre>
     <article html="{{result('#class-template')}}"></article>
@@ -70,9 +70,9 @@ const html = /*html*/`
 <section id="value">
     <h3>Value Binder</h3>
     <template id="value-template">
-    <input value="{{ value.text = $value ?? value.text }}" >
-    <input value="{{ value.text = event ? this.value : value.text }}" >
-    <span>{{value.text}}</span>
+        <input value="{{ value.text = $value ?? value.text }}" >
+        <input value="{{ value.text = event ? this.value : value.text }}" >
+        <span>{{value.text}}</span>
     </template>
     <pre html="{{highlight('#value-template')}}"></pre>
     <article html="{{result('#value-template')}}"></article>
@@ -81,13 +81,13 @@ const html = /*html*/`
 <section id="each">
     <h3>Each Binder</h3>
     <template id="each-template">
-    <div each="{{[ fruits, 'fruit', 'key', 'index' ]}}">
-        <div id="{{fruit}}">
-            <strong>Key: </strong>{{key}},
-            <strong>Index: </strong>{{index}},
-            <strong>Value: </strong>{{fruit}}
+        <div each="{{[ fruits, 'value', 'key', 'index' ]}}">
+            <div id="{{fruit}}">
+                <strong>Key: </strong>{{key}},
+                <strong>Index: </strong>{{index}},
+                <strong>Value: </strong>{{value}}
+            </div>
         </div>
-    </div>
     </template>
     <pre html="{{highlight('#each-template')}}"></pre>
     <article html="{{result('#each-template')}}"></article>
@@ -97,32 +97,32 @@ const html = /*html*/`
     <h3>Select Binder</h3>
 
     <template id="plant-template">
-    <select value="{{plant = $value ?? plant}}">
-        <option value="tree">Tree</option>
-        <option value="cactus">Cactus</option>
-    </select>
-    <span>{{plant}}</span>
+        <select value="{{plant = $value ?? plant}}">
+            <option value="tree">Tree</option>
+            <option value="cactus">Cactus</option>
+        </select>
+        <span>{{plant}}</span>
     </template>
     <pre html="{{highlight('#plant-template')}}"></pre>
     <article html="{{result('#plant-template')}}"></article>
 
     <template id="fruit-template">
-    <select value="{{fruit = $value ?? fruit}}" each="{{[ fruits, 'f' ]}}">
-        <option value="{{f}}">{{f}}</option>
-    </select>
-    <span>{{fruit}}</span>
+        <select value="{{fruit = $value ?? fruit}}" each="{{[ fruits, 'value' ]}}">
+            <option value="{{value}}">{{value}}</option>
+        </select>
+        <span>{{fruit}}</span>
     </template>
     <pre html="{{highlight('#fruit-template')}}"></pre>
     <article html="{{result('#fruit-template')}}"></article>
 
     <template id="cars-template">
-    <select value="{{cars = $value ?? cars}}" multiple>
-        <option value="volvo">Volvo</option>
-        <option value="saab">Saab</option>
-        <option value="opel">Opel</option>
-        <option value="audi">Audi</option>
-    </select>
-    <span>{{cars}}</span>
+        <select value="{{cars = $value ?? cars}}" multiple>
+            <option value="volvo">Volvo</option>
+            <option value="saab">Saab</option>
+            <option value="opel">Opel</option>
+            <option value="audi">Audi</option>
+        </select>
+        <span>{{cars}}</span>
     </template>
     <pre html="{{highlight('#cars-template')}}"></pre>
     <article html="{{result('#cars-template')}}"></article>
@@ -130,8 +130,8 @@ const html = /*html*/`
 
 <section id="html">
     <h3>HTML Binder</h3>
-    <template id="html-template">
-    <div html="{{'<strong>Hyper Text Markup Language</strong>'}}"></div>
+        <template id="html-template">
+        <div html="{{'<strong>Hyper Text Markup Language</strong>'}}"></div>
     </template>
     <pre html="{{highlight('#html-template')}}"></pre>
     <article html="{{result('#html-template')}}"></article>
