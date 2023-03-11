47a8cf97000e9d5beba26ad724b2e1bcdd8d49d5
Alexander Elias <alex.steven.elias@gmail.com>
2023-03-10 17:34:16 -0800

    bug fixes

--- web/guide.js
+++ web/guide.js
@@ -2,100 +2,8 @@ import { component, html } from './x-element.js';
 import Highlight from './modules/highlight.js';
 import Color from './modules/color.js';
 
-const inputComponenet = (self) => html`
-    <div>${self.input}</div>
-    <input value=${self.input} oninput=${(e) => self.input = e.target.value} />
-`;
-
-const mapComponenet = (self) => html`
-    ${self.fruits.map(fruit => html`<div>${fruit}</div>`)}
-`;
-
-const checkComponent = (self) => html`
-    <div>${self.checked ? 'Is Checked' : 'Is Not Checked'}</div>
-    <input type="checkbox" checked=${self.checked} oninput=${(e) => self.checked = e.target.checked} >
-`;
-
-const radioComponenet = (self) => html`
-    <div>${self.radioShared}</div>
-    <input type="radio" name="radio" checked=${self.radioShared === self.radioOne} oninput=${() => self.radioShared = 'one'} />
-    <input type="radio" name="radio" checked=${self.radioShared === self.radioTwo} oninput=${() => self.radioShared = 'two'} />
-`;
-
-const styleComponenet = (self) => html`
-    <div style=${`color: ${self.color}`}>Look at my style</div>
-    <button onclick=${() => self.color = Color()}>Change Color</button>
-`;
-
-const classComponenet = (self) => html`
-    <div class=${self.active ? 'default class-color' : 'default'}>Look at my class</div >
-    <button onclick=${() => self.active = !self.active}>Toggle Class</button>
-`;
-
-const fruitsComponenet = (self) => html`
-    <div>${self.fruit}</div>
-    <select value=${self.fruit} oninput=${(e) => self.fruit = e.target.value}>
-        ${self.fruits.map(fruit => html`
-            <option value=${fruit} selected=${self.fruit===fruit}>${fruit}</option>
-        `)}
-    </select>
-`;
-
-const carsComponenet = (self) => html`
-    <div>${self.car}</div>
-    <select oninput=${(e) => self.car = Array.from(e.target.selectedOptions).map(o => o.value)} multiple>
-        ${self.cars.map(car => html`
-            <option value=${car} selected=${self.car.includes(car)}>${car}</option>
-        `)}
-    </select>
-`;
-
-const selectBooleanComponenet = (self) => html`
-    <div>${self.boolean}</div>
-    <select value=${self.boolean} oninput=${(e) => self.boolean = JSON.parse(e.target.value)}>
-        <option value="true">yes</option>
-        <option value="false">no</option>
-    </select>
-`;
-
-const selectNumberComponenet = (self) => html`
-    <div>${self.number}</div>
-    <select value=${self.number} oninput=${(e) => self.number = JSON.parse(e.target.value)}>
-        <option value="0">zero</option>
-        <option value="1">one</option>
-        <option value="2">two</option>
-    </select>
-`;
-
-const routeCode = Highlight(`;
-import { Router } from './x-element.js';
-
-const main = document.querySelector('main');
-
-const RootContext = () => ({
-    title: 'Root Route'
-});
-
-const RootComponent = ({ h1 }, { title }) => [
-    h1(title)
-];
-
-Router('/', main, RootComponent, RootContext);
-`);
-
-const contextCode = `
-const context = () => ({
-    connect () { console.log('before connect'); },
-    upgrade () { console.log('before upgrade'); },
-    upgraded () { console.log('after upgraded'); },
-    connected () { console.log('after connected'); },
-    disconnect () { console.log('before disconnect'); },
-    disconnected () { console.log('after disconnected'); },
-});
-`;
-
 const names = [
-    'context',
+    'cycle',
     'input',
     'map',
     'check',
@@ -108,20 +16,6 @@ const names = [
     'selectNumber',
     'security',
 ];
-
-const values = {
-    context: Highlight(contextCode),
-    input: Highlight(inputComponenet.toString()),
-    map: Highlight(mapComponenet.toString()),
-    check: Highlight(checkComponent.toString()),
-    radio: Highlight(radioComponenet.toString()),
-    style: Highlight(styleComponenet.toString()),
-    class: Highlight(classComponenet.toString()),
-    fruits: Highlight(fruitsComponenet.toString()),
-    cars: Highlight(carsComponenet.toString()),
-    selectBoolean: Highlight(selectBooleanComponenet.toString()),
-    selectNumber: Highlight(selectNumberComponenet.toString()),
-};
 
 export default component(class XGuide extends HTMLElement {
 
@@ -147,7 +41,7 @@ export default component(class XGuide extends HTMLElement {
             const codeElement = this.querySelector(`#${name}Code`);
             const sourceElement = this.querySelector(`#${name}Source`);
             if (codeElement) {
-                const code = values[ name ];
+                const code = this[`${name}Code`];
                 codeElement.innerHTML = code;
             }
             if (sourceElement) {
@@ -161,6 +55,93 @@ export default component(class XGuide extends HTMLElement {
     disconnecting () { console.log('disconnecting'); }
     disconnected () { console.log('disconnected'); }
 
+    cycleComponent = `
+    class XElement extends HTMLElement {
+        connecting () { console.log('connecting'); }
+        upgrading () { console.log('upgrading'); }
+        upgraded () { console.log('upgraded'); }
+        connected () { console.log('connected'); }
+        disconnecting () { console.log('disconnecting'); }
+        disconnected () { console.log('disconnected'); }
+    }
+    `;
+    cycleCode = Highlight(this.cycleComponent.toString());
+
+    inputComponent = () => html`
+    <div>${this.input}</div>
+    <input value=${this.input} oninput=${(e) => this.input = e.target.value} />
+    `;
+    inputCode = Highlight(this.inputComponent.toString());
+
+    mapComponent = () => html`
+    ${this.fruits.map(fruit => html`<div>${fruit}</div>`)}
+    `;
+    mapCode = Highlight(this.mapComponent.toString());
+
+    checkComponent = () => html`
+    <div>${this.checked ? 'Is Checked' : 'Is Not Checked'}</div>
+    <input type="checkbox" checked=${this.checked} oninput=${(e) => this.checked = e.target.checked} >
+    `;
+    checkCode = Highlight(this.checkComponent.toString());
+
+    radioComponent = () => html`
+    <div>${this.radioShared}</div>
+    <input type="radio" name="radio" checked=${this.radioShared === this.radioOne} oninput=${() => this.radioShared = 'one'} />
+    <input type="radio" name="radio" checked=${this.radioShared === this.radioTwo} oninput=${() => this.radioShared = 'two'} />
+    `;
+    radioCode = Highlight(this.radioComponent.toString());
+
+    styleComponent = () => html`
+    <div style=${`color: ${this.color}`}>Look at my style</div>
+    <button onclick=${() => this.color = Color()}>Change Color</button>
+    `;
+    styleCode = Highlight(this.styleComponent.toString());
+
+    classComponent = () => html`
+    <div class=${this.active ? 'default class-color' : 'default'}>Look at my class</div >
+    <button onclick=${() => this.active = !this.active}>Toggle Class</button>
+    `;
+    classCode = Highlight(this.classComponent.toString());
+
+    fruitsComponent = () => html`
+    <div>${this.fruit}</div>
+    <select value=${this.fruit} oninput=${(e) => this.fruit = e.target.value}>
+        ${this.fruits.map(fruit => html`
+            <option value=${fruit} selected=${this.fruit===fruit}>${fruit}</option>
+        `)}
+    </select>
+    `;
+    fruitsCode = Highlight(this.fruitsComponent.toString());
+
+    carsComponent = () => html`
+    <div>${this.car}</div>
+    <select oninput=${(e) => this.car = Array.from(e.target.selectedOptions).map(o => o.value)} multiple>
+        ${this.cars.map(car => html`
+            <option value=${car} selected=${this.car.includes(car)}>${car}</option>
+        `)}
+    </select>
+    `;
+    carsCode = Highlight(this.carsComponent.toString());
+
+    selectBooleanComponent = () => html`
+    <div>${this.boolean}</div>
+    <select value=${this.boolean} oninput=${(e) => this.boolean = JSON.parse(e.target.value)}>
+        <option value="true">yes</option>
+        <option value="false">no</option>
+    </select>
+    `;
+    selectBooleanCode = Highlight(this.selectBooleanComponent.toString());
+
+    selectNumberComponent = () => html`
+    <div>${this.number}</div>
+    <select value=${this.number} oninput=${(e) => this.number = JSON.parse(e.target.value)}>
+        <option value="0">zero</option>
+        <option value="1">one</option>
+        <option value="2">two</option>
+    </select>
+    `;
+    selectNumberCode = Highlight(this.selectNumberComponent.toString());
+
     template = () => html`
 
         <style>
@@ -173,22 +154,21 @@ export default component(class XGuide extends HTMLElement {
         </style>
 
         <section>
-            <h3>Context</h3>
-            <p>Life cycle methods.</p>
-            <pre id="contextCode"></pre>
+            <h3>Life Cycle</h3>
+            <pre id="cycleCode"></pre>
         </section>
 
         <section id="input">
             <h3>Input</h3>
             <pre id="inputCode"></pre>
-            <pre id="inputComponent">${inputComponenet(this)}</pre>
+            <pre id="inputComponent">${this.inputComponent()}</pre>
             <pre id="inputSource"></pre>
         </section>
 
         <section id="map">
             <h3>Map</h3>
             <pre id="mapCode"></pre>
-            <pre id="mapComponent">${mapComponenet(this)}</pre>
+            <pre id="mapComponent">${this.mapComponent()}</pre>
             <pre id="mapSource"></pre>
         </section>
 
@@ -196,7 +176,7 @@ export default component(class XGuide extends HTMLElement {
             <h3>Check</h3>
             <p>Boolean html attributes will be treated as Boolean paramters and toggle the attribute.</p>
             <pre id="checkCode"></pre>
-            <pre id="checkComponent">${checkComponent(this)}</pre>
+            <pre id="checkComponent">${this.checkComponent()}</pre>
             <pre id="checkSource"></pre>
         </section>
 
@@ -204,21 +184,21 @@ export default component(class XGuide extends HTMLElement {
             <h3>Radio</h3>
             <p>Boolean html attributes will be treated as Boolean paramters and toggle the attribute.</p>
             <pre id="radioCode"></pre>
-            <pre id="radioComponent">${radioComponenet(this)}</pre>
+            <pre id="radioComponent">${this.radioComponent()}</pre>
             <pre id="radioSource"></pre>
         </section>
 
         <section id="class">
             <h3>Class</h3>
             <pre id="classCode"></pre>
-            <pre id="classComponent">${classComponenet(this)}</pre>
+            <pre id="classComponent">${this.classComponent()}</pre>
             <pre id="classSource"></pre>
         </section>
 
         <section id="style">
             <h3>Style</h3>
             <pre id="styleCode"></pre>
-            <pre id="styleComponent">${styleComponenet(this)}</pre>
+            <pre id="styleComponent">${this.styleComponent()}</pre>
             <pre id="styleSource"></pre>
         </section>
 
@@ -226,25 +206,25 @@ export default component(class XGuide extends HTMLElement {
             <h3>Select</h3>
 
             <pre id="fruitsCode"></pre>
-            <pre id="fruitsComponent">${fruitsComponenet(this)}</pre>
+            <pre id="fruitsComponent">${this.fruitsComponent()}</pre>
             <pre id="fruitsSource"></pre>
 
             <br>
 
             <pre id="carsCode"></pre>
-            <pre id="carsComponent">${carsComponenet(this)}</pre>
+            <pre id="carsComponent">${this.carsComponent()}</pre>
             <pre id="carsSource"></pre>
 
             <br>
 
             <pre id="selectBooleanCode"></pre>
-            <pre id="selectBooleanComponent">${selectBooleanComponenet(this)}</pre>
+            <pre id="selectBooleanComponent">${this.selectBooleanComponent()}</pre>
             <pre id="selectBooleanSource"></pre>
 
             <br>
 
             <pre id="selectNumberCode"></pre>
-            <pre id="selectNumberComponent">${selectNumberComponenet(this)}</pre>
+            <pre id="selectNumberComponent">${this.selectNumberComponent()}</pre>
             <pre id="selectNumberSource"></pre>
 
         </section>
--- web/root.js
+++ web/root.js
@@ -33,49 +33,49 @@ export default component(class XRoot extends HTMLElement {
     }
 
     template = () => html`
-    <section>
+        <section>
 
-        <h2>Vision</h2>
-        <h4>X-Element's vision is to provide an agnostic non framework that enhances custom elements with functionality and data binding that mimics native custom element standards.</h4>
+            <h2>Vision</h2>
+            <h4>X-Element's vision is to provide an agnostic non framework that enhances custom elements with functionality and data binding that mimics native custom element standards.</h4>
 
-        <h3>Features</h3>
-        <div class="tiles">
-            <div class="tile">
-                <h4>\u{1F476} Simple</h4>
-                <span>Simple to learning if you know custom elements you know XElement.</span>
+            <h3>Features</h3>
+            <div class="tiles">
+                <div class="tile">
+                    <h4>\u{1F476} Simple</h4>
+                    <span>Simple to learning if you know custom elements you know XElement.</span>
+                </div>
+                <div class="tile">
+                    <h4>\u{1F4E6} Shareable</h4>
+                    <span>A single class to build a single component or an entire app.</span>
+                </div>
+                <div class="tile">
+                    <h4>\u{26A1} Fast</h4>
+                    <span>Tiny footprint ~15KB (minified and compressed).</span>
+                </div>
+                <div class="tile">
+                    <h4>\u{1F477} Framework Agnostic</h4>
+                    <span>Use XElement with any framework - React, Vue, Angular...</span>
+                </div>
+                <div class="tile">
+                    <h4>\u{1F9ED} Client Side Routing</h4>
+                    <span>
+                        Using the new
+                        <a href="https://developer.chrome.com/docs/web-platform/navigation-api/" target="_blank">Navigation API</a>
+                    </span>
+                </div>
             </div>
-            <div class="tile">
-                <h4>\u{1F4E6} Shareable</h4>
-                <span>A single class to build a single component or an entire app.</span>
-            </div>
-            <div class="tile">
-                <h4>\u{26A1} Fast</h4>
-                <span>Tiny footprint ~15KB (minified and compressed).</span>
-            </div>
-            <div class="tile">
-                <h4>\u{1F477} Framework Agnostic</h4>
-                <span>Use XElement with any framework - React, Vue, Angular...</span>
-            </div>
-            <div class="tile">
-                <h4>\u{1F9ED} Client Side Routing</h4>
-                <span>
-                    Using the new
-                    <a href="https://developer.chrome.com/docs/web-platform/navigation-api/" target="_blank">Navigation API</a>
-                </span>
-            </div>
-        </div>
 
-        <h3>Component</h3>
-        <p>
-            Pass a Custom Element Constructor to <code>component()</code> and it be decorated with XElement super powers.
-            Use Template Literal with the <code>html</code> Template Tag to give your Custom Element HTML.
-        </p>
-        <pre id="component"></pre>
+            <h3>Component</h3>
+            <p>
+                Pass a Custom Element Constructor to <code>component()</code> and it be decorated with XElement super powers.
+                Use Template Literal with the <code>html</code> Template Tag to give your Custom Element HTML.
+            </p>
+            <pre id="component"></pre>
 
-        <h3>Router</h3>
-        <pre id="router"></pre>
+            <h3>Router</h3>
+            <pre id="router"></pre>
 
-    </section>
+        </section>
     `;
 
 });
