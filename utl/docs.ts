0e8ccb1c40437e7d4c970f263f05c92aec125e27
Alexander Elias <alex.steven.elias@gmail.com>
2023-05-08 13:44:45 -0700

    removed old script

--- utl/docs.ts
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env -S deno run -A
-
-import { copy, emptyDir } from 'https://deno.land/std@0.152.0/fs/mod.ts';
-
-await copy('./web/index.html', './web/404.html', { overwrite: true });
-await emptyDir('./docs/');
-await copy('./web', './docs', { overwrite: true });
