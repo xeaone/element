eac129a16da456fb0a11655f16b2664179b60f6f
Alexander Elias <alex.steven.elias@gmail.com>
2023-05-08 13:44:56 -0700

    comment

--- utl/watch.ts
+++ utl/watch.ts
@@ -5,18 +5,18 @@ await Deno.writeTextFile('./web/404.html', indexhtml);
 await Deno.writeTextFile('./web/guide/index.html', indexhtml);
 await Deno.writeTextFile('./web/security/index.html', indexhtml);
 
-(await esbuild.context({
-    color: true,
-    bundle: true,
-    sourcemap: true,
-    treeShaking: true,
-    format: 'esm',
-    target: 'esnext',
-    logLevel: 'debug',
-    platform: 'browser',
-    outfile: '../guide/src/deps/x-element.js',
-    entryPoints: [ 'src/index.ts' ],
-})).watch();
+// (await esbuild.context({
+//     color: true,
+//     bundle: true,
+//     sourcemap: true,
+//     treeShaking: true,
+//     format: 'esm',
+//     target: 'esnext',
+//     logLevel: 'debug',
+//     platform: 'browser',
+//     outfile: '../guide/src/deps/x-element.js',
+//     entryPoints: [ 'src/index.ts' ],
+// })).watch();
 
 (await esbuild.context({
     color: true,
